ea68fc954f65e1cf9ecc60922beeeaf3840c4e32
Vladimir Kharseev <vkharseev@gmail.com>
2013-04-05 16:15:21 +0400

    05.04.2013 16:15

--- variables/page_variables.php
+++ variables/page_variables.php
@@ -2,5 +2,12 @@
 	
 	$head_variable = array(
 		'home' => array('title'=>'Загородный клуб «Солнечный»','description'=>''),
+		'about' => array('title'=>'Загородный клуб «Солнечный» :: О клубе','description'=>''),
+		'pool' => array('title'=>'Загородный клуб «Солнечный» :: Бассейн','description'=>''),
+		'restaurant' => array('title'=>'Загородный клуб «Солнечный» :: Ресторан','description'=>''),
+		'child' => array('title'=>'Загородный клуб «Солнечный» :: Детский клуб','description'=>''),
+		'afisha' => array('title'=>'Загородный клуб «Солнечный» :: Афиша','description'=>''),
+		'contacts' => array('title'=>'Загородный клуб «Солнечный» :: Контакты','description'=>''),
+		'bron' => array('title'=>'Загородный клуб «Солнечный» :: Бронирование','description'=>''),
 	);
 ?>
