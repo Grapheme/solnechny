407c18bb0d620f8275eb4b8640a28efbf3d29931
Vladimir Kharseev <vkharseev@gmail.com>
2013-04-05 19:15:40 +0400

    05.04.2013 19:15

--- application/views/guests_interface/includes/scripts.php
+++ application/views/guests_interface/includes/scripts.php
@@ -1,5 +1,5 @@
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script type="text/javascript" src="<?=site_url('js/libs/jquery-1.9.1.min.js');?>"><\/script>')</script>
+<script>window.jQuery || document.write('<script type="text/javascript" src="<?=site_url('js/vendor/jquery-1.9.1.min.js');?>"><\/script>')</script>
 <script type="text/javascript" src="<?=site_url('js/plugins.js');?>"></script>
 <script type="text/javascript" src="<?=site_url('js/main.js');?>"></script>
 <script type="text/javascript" src="<?=site_url('js/index.js');?>"></script>
